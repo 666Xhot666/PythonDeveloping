4fdce620e19ca4e341feac0acab96604578b2089
Konstantin_Gladkiy <666xhot666@gmail.com>
2023-02-15 17:24:16 +0200

    Practice: 2 Task: 1

---
+++ practice_2/task_1/task_1.py
@@ -0,0 +1,36 @@
+from math import sin
+
+
+def int_input(name):
+    return int(input(f'Enter {name} :\t'))
+
+
+def sin_based_function(a, b):
+    return sin(a + b) * sin(a - b)
+
+
+def first_task():
+    print(f'Result :: {sin_based_function(a=int_input("alfa"),b=int_input("beta"))}')
+
+
+def sportsman_function(m, k, checkpoint):
+    if m <= 0 or k <= 0:
+        return 'Never'
+    days = 1
+    while checkpoint > m:
+        checkpoint -= m * k
+        days += 1
+    return days
+
+
+def second_task():
+    checkpoint = 50
+    m, k = int_input('distance of first day'), int_input('percentage of distance increase')/100
+    print(f'The athlete will run more than {checkpoint} km in {sportsman_function(m, k, checkpoint)} days')
+
+
+if __name__ == "__main__":
+    print('....\nRun first task ::>\n')
+    first_task()
+    print('....\nRun second task ::>\n')
+    second_task()
